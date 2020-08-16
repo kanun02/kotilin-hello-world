68d68b5abf70631d3ac85cbaa6a98fae7ff2d616
kanun02 <68175985+kanun02@users.noreply.github.com>
2020-08-16 10:55:06 +0700

    ลบไฟล์ที่ไม่ได้ใช้ทิ้ง

--- day 2/src/main.kt
+++ day 2/src/main.kt
@@ -1,11 +1,1 @@
 fun main() {
-    var x = 1
-
-    while (x < 4) {
-        print(if (x == 1) "Yab" else "Dab")
-            print("ba")
-            x = x + 1
-        }
-        if (x == 4) println ("Do")
-    }
-
